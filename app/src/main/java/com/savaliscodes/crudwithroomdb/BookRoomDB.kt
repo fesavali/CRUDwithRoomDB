debfa3f8f53bbc8de735c8e28b413106335922a8
fesavali <felixnzioki99@gmail.com>
2021-11-30 14:34:16 +0300

    create DB and get instance

---
+++ app/src/main/java/com/savaliscodes/crudwithroomdb/BookRoomDB.kt
@@ -0,0 +1,36 @@
+package com.savaliscodes.crudwithroomdb
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+
+@Database(entities = [BooksDBTable::class], version = 1)
+abstract class BookRoomDB : RoomDatabase() {
+
+    abstract fun bookDao() : BooksDao
+
+
+    companion object {
+        private var bookRoomInstance: BookRoomDB? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: Context): BookRoomDB?{
+            if (bookRoomInstance == null){
+                synchronized(BookRoomDB::class.java){
+                    if (bookRoomInstance == null){
+                        bookRoomInstance = Room.databaseBuilder<BookRoomDB>(
+                                            context.applicationContext,
+                                            BookRoomDB::class.java,
+                                            "book_database")
+                                            .build()
+                    }
+                }
+            }
+            return bookRoomInstance
+        }
+    }
+}
