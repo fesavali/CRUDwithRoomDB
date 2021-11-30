75cd514f7be551673f193b71654d364ecd889147
fesavali <felixnzioki99@gmail.com>
2021-11-30 16:02:53 +0300

    update

--- app/src/main/java/com/savaliscodes/crudwithroomdb/NewBook.kt
+++ app/src/main/java/com/savaliscodes/crudwithroomdb/NewBook.kt
@@ -2,10 +2,16 @@ package com.savaliscodes.crudwithroomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class NewBook : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_book)
+
+        val save = findViewById<Button>(R.id.button)
+        val author = findViewById<EditText>(R.id.author)
+        val title = findViewById<EditText>(R.id.book_title)
     }
 }
