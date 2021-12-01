b25d2ac20090e6516e4288545af079fa1ba40d12
fesavali <felixnzioki99@gmail.com>
2021-12-01 10:04:40 +0300

    remove coroutine import

--- app/src/main/java/com/savaliscodes/crudwithroomdb/BookRoomDB.kt
+++ app/src/main/java/com/savaliscodes/crudwithroomdb/BookRoomDB.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
+
 
 
 @Database(entities = [BooksDBTable::class], version = 1)
